6cbc48da6289f964637067c23139a13471337277
Elizar Pepino <jupenz@gmail.com>
2017-10-30 00:38:52 +0800

    Add random coding quote

--- amigo.go
+++ amigo.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -17,15 +19,15 @@ const tmpl = `{{define "index"}}<!DOCTYPE html>
 	<link href="https://fonts.googleapis.com/css?family=Baloo+Paaji" rel="stylesheet">
 	<style>
 		* {
-			margin  : 0;
+			margin	: 0;
 			padding : 0;
-			border  : none;
+			border	: none;
 			outline : none;
 		}
 
 		body {
 			font-family: Arial, Helvetica, sans-serif;
-			font-size: 18px;
+			font-size: 20px;
 			line-height: 1.5em;
 			background-color: #eaf7f5;
 			color: #888;
@@ -54,12 +56,13 @@ const tmpl = `{{define "index"}}<!DOCTYPE html>
 			margin: 32px auto;
 			border-radius: 10px;
 			width: 400px;
-			height: 300px;
+			min-height: 300px;
 			text-align: center;
 			background: #fff url('https://golang.org/doc/gopher/appenginegopher.jpg') bottom center no-repeat;
 			background-size: 50%;
 			padding: 50px;
 			border: 1px solid rgba(0,0,0,0.2);
+			padding-bottom: 140px;
 		}
 
 		.program {
@@ -74,6 +77,60 @@ const tmpl = `{{define "index"}}<!DOCTYPE html>
 			text-align: center;
 			opacity: 0.7;
 		}
+		blockquote{
+			display:block;
+			background: #fff;
+			padding: 15px 20px 15px 50px;
+			margin: 0 0 20px;
+			position: relative;
+
+			/*Font*/
+			font-size: 18px;
+			line-height: 1.3;
+			color: #666;
+			text-align: left;
+
+
+			/*Box Shadow - (Optional)*/
+			-moz-box-shadow: 2px 2px 15px #efefef;
+			-webkit-box-shadow: 2px 2px 15px #efefef;
+			box-shadow: 2px 2px 15px #efefef;
+		}
+
+		blockquote::before{
+			content: "\201C"; /*Unicode for Left Double Quote*/
+
+			/*Font*/
+			font-size: 60px;
+			font-weight: bold;
+			color: #ccc;
+
+			/*Positioning*/
+			position: absolute;
+			left: 10px;
+			top:0px;
+		}
+
+		blockquote::after{
+			/*Reset to make sure*/
+			content: "";
+		}
+
+		blockquote a{
+			text-decoration: none;
+			background: #eee;
+			cursor: pointer;
+			padding: 0 3px;
+			color: #c76c0c;
+		}
+
+		blockquote a:hover{
+		 color: #666;
+		}
+
+		blockquote em{
+			font-style: italic;
+		}
 	</style>
 </head>
 <body>
@@ -82,13 +139,15 @@ const tmpl = `{{define "index"}}<!DOCTYPE html>
 		<br>
 		<br>
 		<blockquote>
-			A deployed MVP is worth two prototypes.
+			{{ .Proverb }}
 		</blockquote>
 		<br>
 	</main>
 	<footer>
 		<p>
-			<small>&copy; {{.Year}} Elizar Pepino. All rights reserved.</small>
+			<small>
+				A demo page written in Go. Check out source code <a href="https://github.com/elizar/amigo">here</a>
+			</small>
 		</p>
 		<p>
 			<strong>
@@ -111,12 +170,20 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		now := time.Now()
 
+		proverb := "To code or not to code."
+		resp, err := http.Get("http://proverbs-app.antjan.us/random")
+		if err == nil {
+			b, _ := ioutil.ReadAll(resp.Body)
+			proverb = strings.Replace(string(b), "\"", "", -1)
+		}
+
 		t, _ := template.New("amigo").Parse(tmpl)
 		t.ExecuteTemplate(w, "index", struct {
 			Title    string
 			Hostname string
 			Year     int
-		}{"Amigo", host, time.Now().Year()})
+			Proverb  string
+		}{"Amigo", host, time.Now().Year(), proverb})
 
 		elapse := time.Since(now) / time.Millisecond
 		log.Printf("%s %s %dms\n", r.Method, r.RequestURI, elapse)
